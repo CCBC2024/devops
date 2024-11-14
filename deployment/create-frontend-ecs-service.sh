2ef49845b2215157c29e7c241912ff2612d8c4e4
bos-hieu <12520839@gm.uit.edu.vn>
2024-11-13 23:20:31 -0600

    feat: update

--- deployment/create-frontend-ecs-service.sh
+++ deployment/create-frontend-ecs-service.sh
@@ -6,10 +6,14 @@ source ./charity-donation-deployment/common.sh
 # import the variables
 source ./charity-donation-deployment/variables.sh
 
+# Disable pager for aws cli output to avoid manual intervention for large outputs.
+# It is make sure the script runs without interruption
+export AWS_PAGER=""
+
 # define local variables
 revision_number=$(get_task_definition_revision_number $frontend_source_code)
-subnet1_id=$(get_subnet_id $public_subnet1_name)
-subnet2_id=$(get_subnet_id $public_subnet2_name)
+subnet1_id=$(get_subnet_id "$public_subnet1_name")
+subnet2_id=$(get_subnet_id "$public_subnet2_name")
 security_group_id=$(get_security_group_id $security_group_name)
 tg_two_arn=$(get_target_group_arn $frontend_tg_two_name)
 
@@ -22,4 +26,4 @@ sed -e "s|REVISION-NUMBER|$revision_number|g" \
     "$frontend_ecs_service_template_path" > "$frontend_ecs_service_path"
 
 # create frontend ecs service
-create_ecs_service "$frontend_ecs_service_path"
+create_ecs_service "$frontend_source_code" "$frontend_ecs_service_path"
